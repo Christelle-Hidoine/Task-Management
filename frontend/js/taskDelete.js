cd58fa88bad3f94ff9cbdf03fb0c7386383cf599
Christelle-Hidoine <christelle.hidoine@gmail.com>
2023-04-04 18:12:04 +0200

    fonctionnalité delete dans api avec fetch

--- frontend/js/taskDelete.js
+++ frontend/js/taskDelete.js
@@ -10,9 +10,24 @@ const taskDelete = {
         }
     },
 
+    /**
+     * Handler pour supprimer un élément du DOM à la survenue d'un événement (click sur delete)
+     * 
+     * @param {Event} event 
+     */
     handleDeleteTask: function(event) {
+        // on récupère le bouton <div delete> à l'origine de l'event et on remonte à son parent <li>
         currentTask = event.currentTarget.parentNode;
+
+        // on récupère le dataset (data-id) correspondant à ce bouton
+        currentId = currentTask.dataset.id;
+
+        // on supprime la tâche <li> dans le DOM 
         currentTask.remove();
+
+        // on supprime la tâche dans l'API selon l'id récupéré
+        fetch(app.apiConfiguration.endpoint + '/tasks/' + currentId, {method: "DELETE",});
     },
+    
 
 };
